eb92f257de0ba64ac101774eb15918dd3a187d56
joenwoke <jnwoke@hawk.iit.edu>
2025-04-27 09:46:36 -0500

    Aded function for getting bitcoin price

--- OSINT_App.py
+++ OSINT_App.py
@@ -14,3 +14,13 @@ COINCAP_URL = "https://api.coingecko.com/api/v3/coins/bitcoin"
 FEAR_GREED_URL = "https://api.alternative.me/fng/"
 NEWS_API_KEY = os.getenv("NEWS_API_KEY") 
 NEWS_API_URL = f"https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&language=en&pageSize=1&apiKey={NEWS_API_KEY}"
+
+# Functions to fetch data from urls
+def get_bitcoin_price():
+    try:
+        res = requests.get(COINGECKO_URL, timeout=5)
+        res.raise_for_status()
+        price = res.json()["bitcoin"]["usd"]
+        return f"${price:,.2f}"
+    except Exception as e:
+        return f"Price error: {e}"
