8d7973c256d7545100be62e5b4d5315969127234
joenwoke <jnwoke@hawk.iit.edu>
2025-04-26 21:42:22 -0500

    Updated app with API urls

--- OSINT_App.py
+++ OSINT_App.py
@@ -7,3 +7,10 @@ import os
 
 # Load environment variables
 load_dotenv()
+
+# API URLs
+COINGECKO_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+COINCAP_URL = "https://api.coingecko.com/api/v3/coins/bitcoin"
+FEAR_GREED_URL = "https://api.alternative.me/fng/"
+NEWS_API_KEY = os.getenv("NEWS_API_KEY") 
+NEWS_API_URL = f"https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&language=en&pageSize=1&apiKey={NEWS_API_KEY}"
